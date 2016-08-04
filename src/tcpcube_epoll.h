d19803b6dc23d127cd0b4bb941f9335ba2826896
Byeonggon Lee <gonny952@gmail.com>
2016-08-04 21:15:00 +0900

    tcpcube_module_tldestroy()

--- src/tcpcube_epoll.h
+++ src/tcpcube_epoll.h
@@ -14,11 +14,13 @@ struct tcpcube_epoll_module
     void* dl_handle;
     int (*tcpcube_epoll_module_init)(struct tcpcube_module_args*, struct tcpcube_module_list*);
     int (*tcpcube_epoll_module_service)(struct tcpcube_module*, struct tcpcube_epoll_data*);
+    int (*tcpcube_epoll_module_tldestroy)(struct tcpcube_module*);
     int (*tcpcube_epoll_module_destroy)(struct tcpcube_module*);
 };
 
-int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list);
-int tcpcube_epoll_module_start(struct tcpcube_module* module, int* server_socket, pthread_mutex_t* server_socket_mutex);
-int tcpcube_epoll_module_destroy(struct tcpcube_module* module);
+int tcpcube_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list);
+int tcpcube_module_start(struct tcpcube_module* module, int* server_socket, pthread_mutex_t* server_socket_mutex);
+int tcpcube_module_tldestroy(struct tcpcube_module* module);
+int tcpcube_module_destroy(struct tcpcube_module* module);
 
 #endif
